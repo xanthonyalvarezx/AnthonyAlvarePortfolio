d8d27cd3545fc2ec0321631f18d58aa3352235ce
jasperAnonymouse <xanthonyalvarezx@gmail.com>
2024-05-16 12:29:14 -0400

    initial commit

---
+++ src/components/Landing.jsx
@@ -0,0 +1,75 @@
+import "../styles/landing.css";
+import Accordion from "react-bootstrap/Accordion";
+
+import portfolio_image from "../assets/portfolio_image.jpg";
+
+export default function Landing() {
+  return (
+    <>
+      <div class="d-flex flex-column align-items-center m-4 gap-3">
+        <div class="typing">
+          <h1>This is me...</h1>
+        </div>
+        <div id="profile_photo" class="mt-3">
+          <img src={portfolio_image} alt="profile photo" />
+        </div>
+        <div>
+          <p>
+            My Name is Anthony Alvarez. I am a web developer and beginner cyber
+            security analyst.
+            <br />
+          </p>
+        </div>
+        <div class="w-75">
+          <Accordion defaultActiveKey="0" flush>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>My Journey</Accordion.Header>
+              <Accordion.Body>
+                <p>
+                  My journey into the tech world began in 2020, right in the
+                  midst of the pandemic. At the time, I was working in a
+                  warehouse, but I felt a strong pull towards the tech industry.
+                  Thankfully, my employer offered a program that allowed
+                  associates to attend school for free, and I jumped at the
+                  chance. I enrolled in Kenzie Academy's six-month introductory
+                  bootcamp focused on web development basics. I did really well
+                  in the program, which solidified my decision to pursue a
+                  career in tech full-time. So, I left my warehouse job and
+                  devoted myself entirely to my studies.
+                </p>
+                <p>
+                  After a year of intense learning, I graduated with top marks
+                  and earned a certificate in web development. The bootcamp
+                  covered everything from the MERN stack to Python with Django.
+                  Soon after, I landed a position at a startup called
+                  IncubateIQ, where I had the opportunity to build web
+                  applications, including e-commerce and staffing software.
+                  Working alongside a talented team of developers, I thrived in
+                  an environment where we all learned from each other and grew
+                  together.
+                </p>
+                <p>
+                  Within a year, my hard work paid off, and I was promoted from
+                  Junior Developer to Developer. Unfortunately, recent funding
+                  issues led to a layoff at the company. Despite this setback,
+                  I'm excited about finding my next role and continuing my
+                  journey in the tech industry. Whether it's in development or
+                  cybersecurity, which I've been actively teaching myself and
+                  earning certificates in, I'm determined to keep growing and
+                  learning. One of the most important lessons I've learned in
+                  tech is the necessity of never stopping learning.
+                </p>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>What I Offer</Accordion.Header>
+              <Accordion.Body>
+              With my motivated demeanor and unwavering growth mindset, I bring a dynamic energy to any team. As a firm believer in collaboration and collective success, I naturally excel as a team player, leveraging my skills and insights to contribute effectively towards common goals. My commitment to excellence ensures that I consistently strive to surpass expectations in every endeavor, driven by a passion for continuous improvement. Detail-oriented and meticulously organized, I approach tasks with a thoroughness that ensures accuracy and precision in all aspects of my work. Moreover, as a self-starter, I proactively identify opportunities for innovation and take initiative to implement solutions, demonstrating my ability to adapt and thrive in dynamic environments. With these attributes, I am poised to make significant contributions to any company, facilitating growth, efficiency, and success.
+              </Accordion.Body>
+            </Accordion.Item>
+          </Accordion>
+        </div>
+      </div>
+    </>
+  );
+}
---
+++ src/components/NavigationBar.jsx
@@ -0,0 +1,23 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+export default function NavigationBar(){
+  return (
+    <Navbar expand="lg" className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home">Anthony Alvarez</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/Cysec">Cyber Security</Nav.Link>
+            <Nav.Link href="WebDev">Web Development</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
+
